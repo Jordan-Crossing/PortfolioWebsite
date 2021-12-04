c30aa79760905283e869ba62b085f954c19c072b
Jordan-Crossing <80609165+Jordan-Crossing@users.noreply.github.com>
2021-12-04 15:28:28 +1100

    Add random dog photos

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-function cat() {}
+console.log("body");
+
+fetch("https://dog.ceo/api/breeds/image/random")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    let image = document.querySelector("img");
+    image.src = data.message;
+  });
+
+//for each
