0a1f7dd66952c49af4f65331e406b873eb544cb0
Jordan-Crossing <80609165+Jordan-Crossing@users.noreply.github.com>
2021-12-14 04:26:30 +1100

    update

--- my-portfolio/src/App.js
+++
@@ -1,9 +0,0 @@
-import "./App.css";
-import "./components/greeting";
-import greeting from "./components/greeting";
-
-function App() {
-  return <>{greeting("how's it going ?")}</>;
-}
-
-export default App;
