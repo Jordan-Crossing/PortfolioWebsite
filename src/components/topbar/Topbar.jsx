c507ad59a67f521b030547145acf0022ae9af90d
Jordan-Crossing <80609165+Jordan-Crossing@users.noreply.github.com>
2022-02-05 16:26:21 +1100

    Add menu component

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -2,15 +2,15 @@ import "./topbar.scss";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import EmailIcon from "@mui/icons-material/Email";
 
-export default function Topbar() {
+export default function Topbar({ menuOpen, setMenuOpen }) {
   return (
-    <div className="topbar">
+    <div className={"topbar " + (menuOpen && "active")}>
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="logo">
             <span>J</span>
-            <p>ordan</p>
-            <span>.</span>
+            <p>ordan.</p>
+            <span></span>
           </a>
           <div className="linksContainer">
             <a href="https://github.com/Jordan-Crossing">
@@ -25,7 +25,13 @@ export default function Topbar() {
             </a>
           </div>
         </div>
-        <div className="right">R</div>
+        <div className="right">
+          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
       </div>
     </div>
   );
