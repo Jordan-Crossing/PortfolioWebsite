9d6e09247ddc442660629d8eda6d8601b76a0297
Jordan-Crossing <80609165+Jordan-Crossing@users.noreply.github.com>
2022-04-16 14:31:15 +1000

    Add Peanut to intro

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,6 +3,12 @@ import "./intro.scss";
 export default function Intro() {
   return (
     <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="img/PeanutProfile.jpg" alt="cat" />
+        </div>
+      </div>
+      <div className="right">"right"</div>
       <img
         src={require("./layered-waves-haikei.svg").default}
         alt="svg decoration"
