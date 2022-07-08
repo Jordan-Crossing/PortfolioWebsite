9759506d6cabfe843a37b18436c1241f6c6dc9e2
Jordan-Crossing <80609165+Jordan-Crossing@users.noreply.github.com>
2022-07-09 07:50:26 +1000

    Add Showcase

--- src/components/showcase/Showcase.jsx
+++ src/components/showcase/Showcase.jsx
@@ -1,8 +1,23 @@
 import "./showcase.scss";
+import laserCat from "../../assets/LaserCat.jpg";
 
 export default function Showcase() {
   return (
     <div className="showcase" id="showcase">
+      <h1>Showcase</h1>
+      <ul>
+        <li>Featured</li>
+        <li>Featured</li>
+        <li>Featured</li>
+        <li>Featured</li>
+        <li>Featured</li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img src={laserCat} alt="" id="laserCat" />
+          <h3>bank</h3>
+        </div>
+      </div>
       <img
         src={require("./layered-waves-haikei.svg").default}
         alt="svg decoration"
