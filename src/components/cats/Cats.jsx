77d54190d2d1e519cc97c4b7d2fb9db84fafdf68
Jordan-Crossing <80609165+Jordan-Crossing@users.noreply.github.com>
2022-08-23 23:33:59 +1000

    Cat sliders

--- src/components/cats/Cats.jsx
+++ src/components/cats/Cats.jsx
@@ -1,5 +1,22 @@
 import "./cats.scss";
+import { motion } from "framer-motion";
+import { useRef, useState, useEffect } from "react";
+import images from "../../images";
 
 export default function Cats() {
-  return <div className="cats" id="cats"></div>;
+  return (
+    <div className="cats" id="cats">
+      <motion.div className="carousel">
+        <motion.div className="inner-carousel">
+          {images.map((image) => {
+            return (
+              <motion.div className="carousel-item">
+                <img src={image} alt="" />
+              </motion.div>
+            );
+          })}
+        </motion.div>
+      </motion.div>
+    </div>
+  );
 }
---
+++ src/images.js
@@ -0,0 +1,15 @@
+import birdo from "./assets/img/birdo.jpeg";
+import BushTurkey from "./assets/img/BushTurkey.jpeg";
+import Gweela from "./assets/img/Gweela.jpeg";
+import Kookaburra from "./assets/img/Kookaburra.jpeg";
+import PancakeProfile from "./assets/img/PancakeProfile.jpg";
+import PeanutProfile from "./assets/img/PeanutProfile.jpg";
+
+export default [
+  birdo,
+  BushTurkey,
+  Gweela,
+  Kookaburra,
+  PancakeProfile,
+  PeanutProfile,
+];
