469adc9a47008e43e8f169da8abfb90cee1952f9
Jordan-Crossing <80609165+Jordan-Crossing@users.noreply.github.com>
2022-09-02 23:33:15 +1000

    Animate contact card

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,4 +1,5 @@
 import "./contact.scss";
+import { motion } from "framer-motion";
 
 export default function Contact() {
   return (
@@ -7,7 +8,21 @@ export default function Contact() {
       id="contact"
       style={{ backgroundImage: `url(/img/rainbow-stacked-waves-haikei.svg)` }}
     >
-      Contact
+      <motion.div
+        className="contact-card"
+        initial={{ scale: 1, rotate: 355 }}
+        animate={{ rotate: 365, scale: 1 }}
+        transition={{
+          type: "tween",
+          stiffness: 20,
+          damping: 10,
+          repeat: Infinity,
+          duration: 1,
+        }}
+      >
+        <h1>Contact</h1>
+        <p>j.crossing92@gmail.com</p>
+      </motion.div>
     </div>
   );
 }
